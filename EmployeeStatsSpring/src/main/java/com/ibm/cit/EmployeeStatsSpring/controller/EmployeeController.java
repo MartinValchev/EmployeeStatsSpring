571dbfdd0dcafbac2dea03f9b710373afcc0dfe1
MartinValchev <martinvalchev1@gmail.com>
2017-10-23 12:14:35 +0300

    Working on Spring REST

--- EmployeeStatsSpring/src/main/java/com/ibm/cit/EmployeeStatsSpring/controller/EmployeeController.java
+++ EmployeeStatsSpring/src/main/java/com/ibm/cit/EmployeeStatsSpring/controller/EmployeeController.java
@@ -1,30 +1,44 @@
 package com.ibm.cit.EmployeeStatsSpring.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.ibm.cit.EmployeeStatsSpring.model.Employee;
 import com.ibm.cit.EmployeeStatsSpring.service.EmployeeService;
 
 @Controller
-@RequestMapping(path="/employees")
+@RequestMapping(path="/")
 public class EmployeeController {
 	@Autowired
 	@Qualifier("employeeService")
 	private EmployeeService employeeService;
 	
-	@RequestMapping(path="/list")
-	public ModelAndView getEmployees() {
-		List<Employee> employees= employeeService.getEmployees();
+	@RequestMapping(path="/home")
+	public ModelAndView getEmployeesPage() {
 		ModelAndView model = new ModelAndView("home");
-		model.addObject("employees", employees);
-//		Js
-//		model.setView(view);
+		return model;
+	}
+	@RequestMapping(path="/addEmployee")
+	public ModelAndView addEmployeePage() {
+		ModelAndView model = new ModelAndView("AddEmployee");
+		return model;
+	}
+	@RequestMapping(path="/allEmployees")
+	public ModelAndView allEmployeesPage() {
+		ModelAndView model = new ModelAndView("AllEmployees");
+		return model;
+	}
+	
+	@RequestMapping(path="/employee")
+	public ModelAndView getEmployeePage() {
+		ModelAndView model = new ModelAndView("Employee");
+		return model;
+	}
+	@RequestMapping(path="/statistics")
+	public ModelAndView getStatisticsPage() {
+		ModelAndView model = new ModelAndView("Statistics");
 		return model;
 	}
 	
